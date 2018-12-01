59dc407f4cbd16678955d32cc4679e187f1f6289
asmoljo <smoljo.antonio@gmail.com>
2018-12-01 21:51:15 +0100

    Kreiran py file za vjezbanje Lista i sitna izmjena u stringu u StringFormatting_Vjezbanje.py

--- StringFormating_Vjezbanje.py
+++ StringFormating_Vjezbanje.py
@@ -9,4 +9,5 @@ var2 = 'tko zna kad ce opet dobra je...'
 
 print('String here {var1} then also {pozicija2}'.format(var1=s, pozicija2=var2))
 
-print('trla baba lan')
+print('trla baba lan da joj prodje dan')
+
