c6a598aac5d348039fa801ce202e5e76debcb4dc
asmoljo <smoljo.antonio@gmail.com>
2018-12-01 22:46:57 +0100

    nove izmjene

--- StringFormating_Vjezbanje.py
+++ StringFormating_Vjezbanje.py
@@ -1,13 +1,9 @@
 
-
-
 s = 'STRING'
 print('Place another string with a mod and s: %s' %(s))
 
 var2 = 'tko zna kad ce opet dobra je...'
 
-
 print('String here {var1} then also {pozicija2}'.format(var1=s, pozicija2=var2))
 
-print('trla baba lan da joj prodje dan')
-
+print("trla baba lan da joj prodje dan")
